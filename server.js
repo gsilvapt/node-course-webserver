07d04c932acbc3e804018e9345bf552c053b6391
gsilvapt <gustavosantaremsilva@gmail.com>
2018-02-02 15:37:55 +0000

    Removes maintenance parts and adds projects section.

--- server.js
+++ server.js
@@ -29,11 +29,10 @@ app.use((req, res, next) => {
 })
 
 // This bit, since it does not have any next() method, it will block the rest
-// of the code to run.
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-
-});
+// of the code to run. Good for maintenance parts.
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -56,6 +55,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
   });
 });
 
